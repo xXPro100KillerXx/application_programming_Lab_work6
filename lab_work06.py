23c19318a1cf19969a8a5726d303415f84a60c15
xXPro100KillerXx <113775345+xXPro100KillerXx@users.noreply.github.com>
2022-10-16 23:56:25 +0500

    Add files via upload

--- lab_work06.py
+++ lab_work06.py
@@ -5,27 +5,27 @@ for i in range(1, n + 1):       #создание матрицы
     k = 'k'
     c = k + str(i)
     set_k.append(c)
-print(set_k)
+# print(set_k)
 
 for i in range(n):      #создание единичных списков
     set_k[i] = []
     for j in range(n):
         set_k[i].append(1.0)
-print(set_k)
+# print(set_k)
 
 
 for i in range(2, n + 1):       #добавление данных пользователя
     for j in range(1, i):
         x = float(input('Введите ' + str(j) + ' значение для ' + str(i) + ' строки: '))
         set_k[i - 1][j - 1] = x
-print(set_k)
+# print(set_k)
 
 for i in range(n - 2, -1, -1):      #отзеркаливание введенных данных пользователя
     for j in range(i, -1, -1):
         set_k[j][i + 1] = float("{0:.2f}".format(1.0 / set_k[i + 1][j]))
         # set_k[j][i + 1] = 1.0 / set_k[i + 1][j]
-        print(j, i + 1)
-print(set_k)
+        # print(j, i + 1)
+# print(set_k)
 
 
 pr = []
@@ -36,8 +36,8 @@ for i in range(n):      #вычисление сумм строк
         m += set_k[i][j]
         sum += set_k[i][j]
     pr.append(m)
-print(pr)
-print(sum)
+# print(pr)
+# print(sum)
 
 for i in range(n):
     otv = pr[i] / sum
